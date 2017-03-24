fc665a7f2947d0816b0e7747313d65ef2e527831
Rebecca Reed <rreed@andigital.com>
2017-03-24 09:26:51 +0000

    Update the tests to reflect the parameter type

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,12 @@
 module.exports = {
 
-    fizzbuzz: function() 
+    fizzbuzz: function(start, end) 
     {
+        if (isNaN(start) || isNaN(end))
+        {
+            throw new Error ('The start and end parameters need to be numbers');
+        }
+
         return 0;
     }
 
--- test.js
+++ test.js
@@ -10,4 +10,22 @@ describe('Fizz Buzz', function() {
         let f = fizzbuzz.fizzbuzz;
         assert.isFunction(f);
     });
+
+    it('should throw an error if the start and end parameters are not numbers', function() {
+        expect(function() {
+            fizzbuzz.fizzbuzz('a', 'b');
+        }).to.throw('The start and end parameters need to be numbers');
+    });
+
+    it('should throw an error if either the start or end parameters are not numbers', function() {
+        expect(function() {
+            fizzbuzz.fizzbuzz('a', 10);
+        }).to.throw('The start and end parameters need to be numbers');
+    });
+
+    it('should not throw an error if the start or end parameters are numbers', function() {
+        expect(function() {
+            fizzbuzz.fizzbuzz(0, 10);
+        }).to.not.throw('The start and end parameters need to be numbers');
+    });
 });
